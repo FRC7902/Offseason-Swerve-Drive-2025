4dec293cdb4d9d3a64a7e2f04f1103cf4675ea73
Willie Pai <PaisWillie@gmail.com>
2024-10-16 21:38:04 -0400

    feat: add swerve subsystem

---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,93 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+
+public class SwerveSubsystem extends SubsystemBase {
+
+  SwerveDrive m_swerveDrive;
+
+  /** Creates a new SwerveSubsystem. */
+  public SwerveSubsystem() {
+    double maximumSpeed = Units.feetToMeters(4.5); // TODO: Move to constants
+
+    try {
+      File directory = new File(Filesystem.getDeployDirectory(), "swerve");
+      m_swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  /**
+   * Command to drive the robot using translative values and heading as a
+   * setpoint.
+   *
+   * @param translationX Translation in the X direction.
+   * @param translationY Translation in the Y direction.
+   * @param headingX     Heading X to calculate angle of the joystick.
+   * @param headingY     Heading Y to calculate angle of the joystick.
+   * @return Drive command.
+   */
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier headingX,
+      DoubleSupplier headingY) {
+    return run(() -> {
+      double xInput = Math.pow(translationX.getAsDouble(), 3); // Smooth controll out
+      double yInput = Math.pow(translationY.getAsDouble(), 3); // Smooth controll out
+      // Make the robot move
+      driveFieldOriented(m_swerveDrive.swerveController.getTargetSpeeds(xInput, yInput,
+          headingX.getAsDouble(),
+          headingY.getAsDouble(),
+          m_swerveDrive.getYaw().getRadians(),
+          m_swerveDrive.getMaximumVelocity()));
+    });
+  }
+
+  /**
+   * Command to drive the robot using translative values and heading as angular
+   * velocity.
+   *
+   * @param translationX     Translation in the X direction.
+   * @param translationY     Translation in the Y direction.
+   * @param angularRotationX Rotation of the robot to set
+   * @return Drive command.
+   */
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY,
+      DoubleSupplier angularRotationX) {
+    return run(() -> {
+      // Make the robot move
+      m_swerveDrive.drive(new Translation2d(translationX.getAsDouble() * m_swerveDrive.getMaximumVelocity(),
+          translationY.getAsDouble() * m_swerveDrive.getMaximumVelocity()),
+          angularRotationX.getAsDouble() * m_swerveDrive.getMaximumAngularVelocity(),
+          true,
+          false);
+    });
+  }
+
+  /**
+   * Drive the robot given a chassis field oriented velocity.
+   *
+   * @param velocity Velocity according to the field.
+   */
+  public void driveFieldOriented(ChassisSpeeds velocity) {
+    m_swerveDrive.driveFieldOriented(velocity);
+  }
+}
